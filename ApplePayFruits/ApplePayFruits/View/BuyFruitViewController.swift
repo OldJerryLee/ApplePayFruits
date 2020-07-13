7046244154a484b41191a59f5e9c407996bb192e
henriqueconte <henriqueconte6@gmail.com>
2020-07-13 20:01:41 -0300

    Implemented iOS Apple Pay code

--- ApplePayFruits/ApplePayFruits/View/BuyFruitViewController.swift
+++ ApplePayFruits/ApplePayFruits/View/BuyFruitViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PassKit
 
 class BuyFruitViewController: UIViewController {
 
@@ -47,7 +48,46 @@ class BuyFruitViewController: UIViewController {
     }
 
     @IBAction func purchase(sender: UIButton) {
-        // TODO: - Fill in implementation
+        
+        //1
+        let paymentItem = PKPaymentSummaryItem.init(label: fruit.title, amount: fruit.price)
+        
+        //2
+        let paymentMethods: [PKPaymentNetwork] = [PKPaymentNetwork.amex, .visa, .masterCard, .elo]
+        
+        if PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: paymentMethods) {
+            
+            let request = PKPaymentRequest()
+            request.currencyCode = "USD" // 1
+            request.countryCode = "US" // 2
+            request.merchantIdentifier = "merchant.com.appFruitsTest.ApplePayFruits" // 3
+            request.merchantCapabilities = .capability3DS // 4
+            request.supportedNetworks = paymentMethods // 5
+            request.paymentSummaryItems = [paymentItem] // 6
+            
+            guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request) else {
+                print("Failed opening authorization view controller")
+                return
+            }
+            paymentVC.delegate = self
+            self.present(paymentVC, animated: true, completion: nil)
+        }
+        else {
+            print("nao consegue fazer pagamentos")
+        }
     }
+    
+}
 
+extension BuyFruitViewController: PKPaymentAuthorizationViewControllerDelegate {
+
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
+        
+        completion(.success)
+    }
+    
 }
